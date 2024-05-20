bd0959298b9c2a7e613ba06e2737bed1cd378512
erkebulan777@gmail.com <erkebulan777@gmail.com>
2024-05-20 19:13:42 +0500

    555

--- PrintUtil/PdfMergeHandler.cs
+++ PrintUtil/PdfMergeHandler.cs
@@ -9,62 +9,64 @@ using Document = iTextSharp.text.Document;
 namespace RevitBIMTool.PrintUtil;
 internal static class PdfMergeHandler
 {
+
+
     public static void CombinePDFsFromFolder(List<SheetModel> sheetModels, string directory, string outputFullName, bool deleted = true)
     {
         if (File.Exists(outputFullName)) { File.Delete(outputFullName); }
 
-        using (FileStream stream = new FileStream(outputFullName, FileMode.Create))
+        using FileStream stream = new(outputFullName, FileMode.Create);
+        Document outputDocument = new();
+
+        PdfCopy copy = new PdfSmartCopy(outputDocument, stream);
+
+        outputDocument.Open();
+
+        List<SheetModel> sheetModelList = SortSheetModels(sheetModels);
+
+        foreach (SheetModel model in sheetModelList)
         {
-            Document outputDocument = new Document();
+            string filePath = model.FindFileInDirectory(directory);
 
-            PdfCopy copy = new PdfSmartCopy(outputDocument, stream);
-
-            outputDocument.Open();
-
-            foreach (SheetModel model in SortSheetModels(sheetModels))
+            if (!string.IsNullOrEmpty(filePath))
             {
-                string filePath = model.FindFileInDirectory(directory);
+                PdfReader reader = new(filePath);
+                reader.ConsolidateNamedDestinations();
 
-                if (!string.IsNullOrEmpty(filePath))
+                try
                 {
-                    PdfReader reader = new PdfReader(filePath);
-                    reader.ConsolidateNamedDestinations();
-
-                    try
+                    for (int num = 1; num <= reader.NumberOfPages; num++)
                     {
-                        for (int num = 1; num <= reader.NumberOfPages; num++)
+                        PdfImportedPage page = copy.GetImportedPage(reader, num);
+
+                        if (page != null && outputDocument.IsOpen())
                         {
-                            PdfImportedPage page = copy.GetImportedPage(reader, num);
-
-                            if (page != null && outputDocument.IsOpen())
-                            {
-                                copy.AddPage(page);
-                            }
+                            copy.AddPage(page);
                         }
-
-                        copy.FreeReader(reader);
-
                     }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine(ex);
-                    }
-                    finally
-                    {
-                        reader.Close();
-                        model.Dispose();
 
-                        if (deleted)
-                        {
-                            File.Delete(filePath);
-                        }
+                    copy.FreeReader(reader);
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+                finally
+                {
+                    reader.Close();
+                    model.Dispose();
+
+                    if (deleted)
+                    {
+                        File.Delete(filePath);
                     }
                 }
             }
-
-            copy.Close();
-            outputDocument.Close();
         }
+
+        copy.Close();
+        outputDocument.Close();
     }
 
 
