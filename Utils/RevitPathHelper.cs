f4dddd5309a53b1df7d1f19e1676d67f4569121d
erkebulan777@gmail.com <erkebulan777@gmail.com>
2024-08-28 13:00:56 +0500

    BIM

--- Utils/RevitPathHelper.cs
+++ Utils/RevitPathHelper.cs
@@ -10,7 +10,7 @@ namespace RevitBIMTool.Utils;
 public static class RevitPathHelper
 {
 
-    private static readonly string[] sectionAcronyms = { "AR", "AS", "KJ", "KR", "KG", "OV", "VK", "EOM", "EM", "PS", "SS", "OViK", "APT" };
+    private static readonly string[] sectionAcronyms = { "AR", "AS", "KJ", "KR", "KG", "OV", "VK", "EOM", "EM", "PS", "SS", "OViK", "APT", "BIM" };
 
 
     public static string GetUNCPath(string inputPath)
