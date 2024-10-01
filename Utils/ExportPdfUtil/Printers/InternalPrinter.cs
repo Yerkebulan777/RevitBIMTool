4b50f6aeb04a8cbaf91f86f3628046b753ed6381
erkebulan777@gmail.com <erkebulan777@gmail.com>
2024-10-02 03:31:29 +0500

    InternalPrinter

---
+++ Utils/ExportPdfUtil/Printers/InternalPrinter.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.DB;
+using System.IO;
+
+
+namespace RevitBIMTool.Utils.ExportPdfUtil.Printers
+{
+    internal sealed class InternalPrinter
+    {
+        public bool ExportSheet(Document doc, ViewSheet viewSheet, string filePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string directory = Path.GetDirectoryName(filePath);
+
+            PDFExportOptions option = new()
+            {
+                FileName = fileName
+            };
+
+            IList<ElementId> viewIds = [viewSheet.Id];
+
+            if (doc.Export(directory, viewIds, option))
+            {
+                Thread.Sleep(100);
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
