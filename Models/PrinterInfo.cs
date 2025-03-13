2ca8b07a4cfebcf5ace0b96264978424256d5311
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-03-13 16:24:54 +0500

    555

---
+++ Models/PrinterInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace RevitBIMTool.Utils.ExportPDF;
+
+internal static partial class PrinterMemoryManager
+{
+    // Структура для хранения информации о принтерах
+    private class PrinterInfo
+    {
+        public string PrinterName { get; set; }
+        public DateTime LastSuccessTime { get; set; }
+        public int SuccessCount { get; set; }
+    }
+}
