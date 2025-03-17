e94c1a239aa957948e5324eb81c515305757bbd6
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-03-17 23:47:03 +0500

    555

--- Utils/ExportPDF/Printers/InternalPrinter.cs
+++ Utils/ExportPDF/Printers/InternalPrinter.cs
@@ -8,7 +8,6 @@ namespace RevitBIMTool.Utils.ExportPDF.Printers;
 
 internal sealed class InternalPrinter : PrinterControl
 {
-
     public override string RegistryPath => "Undefined registry path";
     public override string PrinterName => "Internal Printer";
     public override string RevitFilePath { get; set; }
