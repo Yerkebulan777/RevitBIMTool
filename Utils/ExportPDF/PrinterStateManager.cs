213607d24465a833fb78c02590baa906d0da456c
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-24 12:53:04 +0500

    12345679

--- Utils/ExportPDF/PrinterStateManager.cs
+++ Utils/ExportPDF/PrinterStateManager.cs
@@ -2,7 +2,6 @@
 using RevitBIMTool.Utils.ExportPDF.Printers;
 using RevitBIMTool.Utils.SystemHelpers;
 using Serilog;
-using System.IO;
 using System.Xml.Serialization;
 
 namespace RevitBIMTool.Utils.ExportPDF;
