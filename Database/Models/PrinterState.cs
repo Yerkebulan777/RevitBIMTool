d164c1dcdf254e2c5b7218b680a2ae1660f08e0b
Yerkebulan <42465235+Yerkebulan777@users.noreply.github.com>
2025-07-24 14:23:32 +0500

    4.8

--- Database/Models/PrinterState.cs
+++ Database/Models/PrinterState.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RevitBIMTool.Database.Models
@@ -8,7 +9,6 @@ namespace RevitBIMTool.Database.Models
     /// Использует оптимистичное блокирование через RowVersion
     /// </summary>
     [Table("printer_states")]
-
     public class PrinterState
     {
         [Key]
